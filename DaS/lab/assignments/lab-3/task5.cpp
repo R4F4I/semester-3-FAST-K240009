9df6b1852589af10608126dcb8db0442dfb11ce7
R4F4I <rafay062005@gmail.com>
2025-09-15 21:44:37 +0500

    completed two

--- DaS/lab/assignments/lab-3/task5.cpp
+++ DaS/lab/assignments/lab-3/task5.cpp
@@ -24,11 +24,197 @@ using namespace std;
 // for palindrome checking, reach the middle of LL, then 'break' the LL, then reverse the end half of the LL, then compare from the two ends, 
 
 
+class singlyNode
+{
+public:
+    int data;
+    singlyNode *next;
+
+    // constructor
+    singlyNode(int data){
+        this->data = data;
+        next = nullptr;
+    }
+    singlyNode(){
+        this->data = 0;
+        next = nullptr;
+    }
+    void print(){
+        std::cout << this->data << " "<< this->next << std::endl;
+    }
+};
+class singly_LL
+{
+public:
+    singlyNode* head;
+    int length;
+    singly_LL(){
+        head = nullptr;
+    }
+    // insert a data at the end of the node
+    void insert_end(int data){
+        // allocate memory (using new) of size node, return type pointer of node, why? to prevent the local var from being destroyed when the function ends (DMA prevents the var from being destroyed automatically) 
+        // also new_node is pointer to this DMA created node, it contains the node's address not its value
+        singlyNode *new_node = new singlyNode(data);
+        
+        // empty LL checker
+        if (head == nullptr) // the value of head ptr
+        {
+            head = new_node; // copy the new_nodes address into head
+            return;
+        }
+
+        // traverse to the end of ll
+        
+        singlyNode *temp = head; // copy head pointer's value into temp
+        while (temp->next!=nullptr)
+        {
+            // temp->next is sam (*temp).next, meaning temp is being dereferenced and the next value of that node is being store in the temp pointer
+            temp = temp->next;
+        }
+        // now that temp is null, get it to store the new_node's address
+        temp->next = new_node;
+    }
+    
+    // insert the node in the end of the LL
+    void insert_node(singlyNode* node){
+        // traverse to the end of node
+        if (head == nullptr)
+        {
+            // std::cout << "1st node" << std::endl;
+            head = node;
+        } else{
+            singlyNode *temp = head; 
+            while (temp->next!=nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = node;
+        }
+    }
+
+    
+    // return the address of the node rather than deleting it
+    singlyNode* extract_pos(int pos){
+        if (head!=nullptr)
+        {
+            std::cout << "pos: " <<pos<< std::endl;
+            singlyNode *prev = nullptr, *curr = nullptr, *next = head;
+            std::cout << "pos: " <<pos<< std::endl;
+            while (pos-- != 0)
+            {
+                std::cout << "pos: " <<pos<< std::endl;
+                prev = curr;
+                curr = next;
+                next = next->next;
+            }
+            // get the prev node to point to next node, leaving the middle node dangling
+            if (prev!=nullptr)
+            {
+                prev->next = next;
+                // return the dangling node
+                std::cout << "curr: " <<curr->data<< std::endl;
+                return curr;
+            } else {
+                return curr;
+            }
+            return nullptr;
+        }
+        return nullptr;
+    }
+    
+    bool isPalindrome(){
+        // create a copy of the original LL
+        singly_LL ll;
+        singlyNode* temp=head; // point to the original LL head
+        singlyNode* node = new singlyNode(*temp);
+        while (temp!=nullptr)
+        {
+            node = temp; // copy the node BY VAL, temp is pointing into 'node'
+            temp = temp->next; // traverse
+            // make node point to null
+            node->next = nullptr;
+            ll.insert_node(node);
+        }
+        
+        // reach to the middle of LL using slowP, fastP approach
+
+        singlyNode *slowP =ll.head; 
+        singlyNode *fastP = ll.head;
+
+        while (fastP != nullptr && fastP->next !=nullptr)
+        {
+            slowP = slowP->next;
+            fastP = fastP->next->next;
+        }
+        // reverse LL from middle to end
+        fastP = ll.reverse_LL(slowP);
+        // now since the fastP is at end, compare the nodes from head and fastP
+        temp = head;
+        while (temp!=fastP && temp!=nullptr && fastP != nullptr)
+        {
+            temp = temp->next;
+            fastP = fastP->next;
+
+            if (temp->data != fastP->data)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // will not make the last node point to null
+    singlyNode* reverse_LL(singlyNode* start){
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        // need 3 pointers
+        singlyNode *prev=nullptr, *curr=nullptr, *next=start; // reverse LL from the starting point
+
+        while (next!=nullptr)
+        {
+            // progress forward or 'crawl' through the LL
+            prev = curr;
+            curr = next;
+            next = next->next;
+
+            // reverse the pointer of the curr node
+            curr->next = prev;
+        }
+        // return the pointer to the new beginning
+        return curr;
+        
+    }
+
+
+    void print(){
+        singlyNode *temp = head; // a temporary pointer to traverse through the LL
+        while (temp!=nullptr)
+        {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout <<"NULL"<< std::endl;
+        
+    }
+};
+
+
 
 int main(){
 
 
+    singly_LL ll;
+    ll.insert_end(1);
+    ll.insert_end(0);
+    ll.insert_end(2);
+    ll.insert_end(0);
+    ll.insert_end(1);
+    ll.print();
 
+    std::cout << "is palindrome:" << ll.isPalindrome()<<std::endl;
 
     return 0;
 }
--- DaS/lab/assignments/lab-3/task7.cpp
+++ DaS/lab/assignments/lab-3/task7.cpp
@@ -20,10 +20,176 @@ e. Print the complete circular link list.
 using namespace std;
 
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data){
+        this->data = data;
+        next = nullptr;
+    }
+    Node(){
+        this->data = 0;
+        next = nullptr;
+    }
+};
+class circ_LL
+{
+public:
+    Node *head, *tail;
+    int length;
+    circ_LL(){
+        head = nullptr;
+        tail = nullptr;
+    }
+
+
+    // same as singly LL, except last node will point to the new node
+    void insert_start(int data){
+        
+        Node *new_node = new Node(data);
+        
+        // empty LL checker
+        if (head == nullptr) // the value of head ptr
+        {
+            head = new_node;
+            return;
+        } else {
+            new_node->next = head; // copy the value of head / new_node is pointing where is head is pointing
+            head = new_node; // head now points to the new node
+            tail->next = new_node; // last node points to the new node as well
+            return;
+        }
+    }
+
+
+    // insert a data at the end of the node
+    void insert_end(int data){
+        // allocate memory (using new) of size node, return type pointer of node, why? to prevent the local var from being destroyed when the function ends (DMA prevents the var from being destroyed automatically) 
+        // also new_node is pointer to this DMA created node, it contains the node's address not its value
+        Node *new_node = new Node(data);
+        
+        // empty LL checker
+        if (head == nullptr) // the value of head ptr
+        {
+            head = new_node; // copy the new_nodes address into head
+            tail = new_node;
+            return;
+        }
+
+        // since tail is pointing to last node, just use it instead of traversal
+        tail->next = new_node;
+        // tail points to the new node
+        tail = new_node;
+        // since this is a circ ll, the last node will point to first node
+        new_node->next = head;
+
+    }
+
+
+    // delete the first node (conserve memory)
+    void remove_start(){
+        
+        Node *temp = head; // temp will point to first node
+        
+        // the head will simply point to the next node
+        head = head->next;
+
+        // the last node will point to this next node
+        tail->next = head;
+        
+        delete temp; // deleting the dangling node
+    }
+
+
+    // 2nd last node will simply point to first node
+    void remove_end(){
+        if (head!=nullptr)
+        {
+            Node *prev=nullptr, *next=head;
+            // traverse to end with 2 pointers
+            while (next!=tail) // check whether the next pointer is pointing to the last node
+            {
+                prev = next;
+                next = next->next;
+            }
+            prev->next = head;
+            tail = prev;
+            // this will leave a dangling node
+
+            delete next; // deleting the dangling node
+        }
+    }
+
+    // remove node at nth position
+    void remove_pos(int pos){
+        if(pos == 1){
+            remove_start();
+            return;
+        }
+        if (head!=nullptr)
+        {
+            Node *prev = nullptr, *curr = nullptr, *next = head;
+            while (pos-- != 0)
+            {
+                prev = curr;
+                curr = next;
+                next = next->next;
+            }
+            // get the prev node to point to next node, leaving the middle node dangling
+
+            prev->next = next;
+
+            // delete the dangling node
+
+            delete curr;
+        }
+    }
+
+
+    // will point until the node at which tail is pointing is reached
+    void print(){
+        Node *temp = head; // a temporary pointer to traverse through the LL
+        
+        do
+        {
+            std::cout << temp->data << " -> ";
+            // std::cout << "\" "<< temp->data << "  " << head->data << " \"";
+            temp = temp->next;
+        }
+        while (temp!=head); // traverse until it circles back
+        std::cout <<"head"<< std::endl;
+        
+    }
+};
 
 
 
 int main(){
+
+    circ_LL ll;
+
+    ll.insert_end(2);
+    ll.insert_end(3);
+    ll.insert_end(4);
+    ll.print();
+    ll.insert_end(5);
+    ll.print();
+    ll.insert_start(6);
+    ll.insert_start(7);
+    ll.insert_start(8);
+    ll.insert_start(9);
+    ll.print();
+
+    ll.remove_pos(3);
+    ll.print();
+    ll.remove_end();
+    ll.print();
+    ll.remove_start();
+    ll.print();
 
 
 
