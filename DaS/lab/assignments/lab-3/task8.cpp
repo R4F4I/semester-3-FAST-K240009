8c5d4cf27db47ad67f11d0660b428ef099c81a9d
R4F4I <rafay062005@gmail.com>
2025-09-16 19:35:33 +0500

    completed task 8

--- DaS/lab/assignments/lab-3/task8.cpp
+++ DaS/lab/assignments/lab-3/task8.cpp
@@ -12,16 +12,159 @@ nodes, into a single list that contains all the nodes of L followed by all the n
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+
+
+class doublyNode
+{
+public:
+    int data;
+    doublyNode *prev, *next;
+
+    // constructor
+    doublyNode(int data){
+        this->data = data;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+
+class doubly_LL
+{
+public:
+    doublyNode *head, *tail;
+
+    doubly_LL(){
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insert_end(int data){
+        doublyNode *new_node = new doublyNode(data);
+        if (head==nullptr)
+        {
+            head = new_node;
+            new_node->prev = nullptr; // first node has no prev node to point to
+            tail = new_node;
+            return;
+        }
+
+        // tail points to last node
+
+        tail->next = new_node; // last node point to new node
+        new_node->prev = tail; // new node points to prev node
+        tail = new_node; // tail points to new node
+
+    }
+
+    void insert_start(int data){
+        doublyNode *node = new doublyNode(data);
+
+        // new node will point to where head is pointing
+        node->next = head;
+        // head will point to new node
+        head = node;
+    }
+
+    void reverse_LL(){
+        // simply switch the prev pointer, and next pointer values
+        // point head to the last node
+        // a single temp pointer will traverse through the LL, and swap the pointers, the last node will be head
+
+        doublyNode *temp = head, *swap;
+        while (temp->next!=nullptr)
+        {
+        
+            // swap
+            swap = temp->next;
+            temp->next = temp->prev;
+            temp->prev = swap;
+            
+            temp = temp->prev;
+        }
+        
+        // after reaching last node, perform last swap
+        swap = temp->next;
+        temp->next = temp->prev;
+        temp->prev = swap;
+
+        doublyNode *newTemp;
+
+        // head and tail will switch places
+        newTemp = head;
+        head = tail;
+        tail = newTemp;
+        
+    }
+    void printNode(doublyNode* node){
+        std::cout << "node: ";
+        std::cout << std::setw(13) << node->prev << " | " << node->data << " | " << node->next << std::setw(13) << "    <--- " << node << std::endl;
+    }
+
+    // merge this ll into the object
+    void merge(doubly_LL ll){
+
+        // for this get the one calling is L, and the arg is M
+        // get tail of L to point to the M head node, and the head node to point to L's tail
+        tail->next = ll.head;
+        ll.head->prev = tail;
+        // the M tail will be new tail
+        tail = ll.tail;
+    }
+
+    void print(){
+        doublyNode *temp = head; // a temporary pointer to traverse through the LL
+
+        while (temp!=nullptr)
+        {
+            if (temp == head)
+            {
+                // std::cout << "head->" << head << std::endl;
+                std::cout << std::setw(12) <<"head-> "  <<  temp->prev << " | " << temp->data << " | " << temp->next << std::setw(15) << "    <--- " << temp << std::endl;
+            }else
+            {
+                std::cout << std::setw(13) << temp->prev << " | " << temp->data << " | " << temp->next << std::setw(13) << "    <--- " << temp << std::endl;
+            }
+            
+            
+
+            temp = temp->next;
+        }
+        
+    }
+};
 
 
 
 
 int main(){
 
+    doubly_LL L;
+    L.insert_end(2);
+    L.insert_end(3);
+    L.insert_end(4);
+    L.insert_end(5);
 
+    L.print();
+
+    std::cout << "---------" << std::endl;
+    
+    doubly_LL M;
+    M.insert_end(6);
+    M.insert_end(7);
+    M.insert_end(8);
+    M.insert_end(9);
+    
+    M.print();
+    std::cout << "---------" << std::endl;
+
+    L.merge(M);
+
+    L.print();
 
 
     return 0;
--- DaS/lab/assignments/lab-3/task9.cpp
+++ DaS/lab/assignments/lab-3/task9.cpp
@@ -1,8 +1,8 @@
 /*
 
 Task#9:
-Given a linked list, you have to perform the following task:
-1. Extract the alternative nodes starting from second node.
+Given a linked list, you have to perform the following task:
+1. Extract the alternative nodes starting from second node.
 2. Reverse the extracted list.
 3. Append the extracted list at the end of the original list.
 Note: Try to solve the problem without using any extra memory.
@@ -26,11 +26,172 @@ appending them to the end of the list results in a list 10->9->3->9->4->5->1->4.
 
 using namespace std;
 
+// extract the nodes in alternating fashion, place these extracted nodes into a new LL, reverse that LL, then merge it
+// for effiecney this ll has tail pointer
+
+
+class singlyNode
+{
+public:
+    int data;
+    singlyNode *next;
+
+    // constructor
+    singlyNode(int data){
+        this->data = data;
+        next = nullptr;
+    }
+    singlyNode(){
+        this->data = 0;
+        next = nullptr;
+    }
+};
+class singly_LL
+{
+public:
+    singlyNode *head, *tail;
+    int length;
+    singly_LL(){
+        head = nullptr;
+    }
+
+    void checkLength(){ // of LL
+        singlyNode *temp=head;
+        int i =0;
+        while(temp!=nullptr){
+            temp = temp->next;
+            i++;
+        }
+        this->length = i;
+    }
+
+    void merge(singlyNode ll){
+
+    }
+
+    void insert_end(int data){
+        singlyNode *new_node = new singlyNode(data);
+        
+
+        if (head == nullptr) 
+        {
+            head = new_node; 
+            tail = new_node;
+            return;
+        }
+
+        tail->next = new_node; // get last node to point to new node
+        tail = new_node; // tail now points to new last node
+    }
+
+    // insert the node in the end of the LL
+    void insert_node(singlyNode* node){
+
+        // cout<<"insert"<<endl;
+        if (head == nullptr)
+        {
+            head = node;
+            tail = node;
+            return;
+        }
+        
+
+        tail->next = node;
+        tail = node;
+    }
+
+    
+    // return the address of the node rather than deleting it, assume, if pos is last, get tail to retract, for this remove 
+    singlyNode* extract_pos(int pos){
+        checkLength();
+        // if it is the last node to be extracted
+        if (pos == this->length)
+        {
+            // the position is the last node, for this simply get the 2nd last node to point to null 
+            singlyNode *curr = nullptr, *next = head;
+            while (next->next != nullptr)
+            {
+                curr = next;
+                next = next->next;
+            }
+            tail = curr;
+            tail->next = nullptr;
+
+            return next;
+        }
+        
+        if (head!=nullptr)
+        {
+            singlyNode *prev = nullptr, *curr = nullptr, *next = head;
+
+            while (pos-- != 0)
+            {
+                prev = curr;
+                curr = next;
+                next = next->next;
+            }
+            // get the prev node to point to next node, leaving the middle node dangling
+            if (prev!=nullptr)
+            {
+                
+                prev->next = next;
+                // return the dangling node
+                
+                std::cout << "curr: " <<curr->data<< std::endl;
+                return curr;
+            } else {
+                return curr;
+            }
+            return nullptr;
+        }
+        return nullptr;
+    }
+
+    void print(){
+        singlyNode *temp = head; // a temporary pointer to traverse through the LL
+        while (temp!=nullptr)
+        {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout <<"NULL"<< std::endl;
+        
+    }
+};
 
 
 
 
 int main(){
+
+    singly_LL ll;
+
+    ll.insert_end(10);
+    ll.insert_end(4);
+    ll.insert_end(9);
+    ll.insert_end(1);
+    ll.insert_end(3);
+    ll.insert_end(5);
+    ll.insert_end(9);
+    ll.insert_end(4);
+
+    ll.print();
+
+    // extract alternative nodes and place them into a new ll
+
+    singly_LL newLL;
+    singlyNode *tempNode;
+
+    for (size_t i = 1; i < 9; i++)
+    {
+        if (i%2==0)
+        {
+            tempNode = ll.extract_pos(i);
+            newLL.insert_node(tempNode);
+        }
+        
+    }
+    newLL.print();
 
 
 
