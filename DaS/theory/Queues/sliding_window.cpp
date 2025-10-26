7234ecec40baf70d24c71978834f85b948724c71
R4F4I <rafay062005@gmail.com>
2025-10-26 21:09:58 +0500

    completed

--- DaS/theory/Queues/sliding_window.cpp
+++ DaS/theory/Queues/sliding_window.cpp
@@ -1,5 +1,9 @@
 /*
 
+suppose an arr, with val k, k is a window of size < arr.size
+
+k will slide through the arr, and create a sum and place it in new,
+this will create a new arr of same size that will have all the slided sums, find the largest val
 
 
 * Name:         Rafay Siddiqui
@@ -14,10 +18,6 @@
 using namespace std;
 
 
-// suppose an arr, with val k, k is a window of size < arr.size
-
-// k will slide through the arr, and create a sum and place it in new,
-// this will create a new arr of same size that will have all the slided sums, find the largest val
 
 class Node{
 
@@ -33,10 +33,30 @@ Node(int data=0){
     }
 };
 
-class Q {
+class Queue {
     
 public:
     Node *head, *tail;
+    int size;
+
+    Queue(int size = 0){
+      head=nullptr;
+      tail=nullptr;  
+      this->size = size;
+    }
+
+    // current number of items in Queue
+    int  population(){
+        Node* temp = head;
+        int i = 1;
+        while (temp!=tail)
+        {
+            temp = temp->next;
+            i++;
+        }
+        return i;
+    }
+
 
     void Enq(int data){
 
@@ -49,9 +69,19 @@ public:
             return;
         }
 
+        if (population()>size)
+        {
+            
+            std::cout << "queue overflow!" << std::endl;
+            // onw deq a val to enq
+            Deq();
+        }
         head->prev = newNode;
         newNode->next = head;
         head = newNode;
+        
+        
+
 
     }
     int Deq(){
@@ -79,11 +109,66 @@ public:
         return sum;
     }
 
+    void print(){
+        Node* temp = head;
+        while (temp!=tail)
+        {
+            std::cout << temp->data <<" ";
+            temp = temp->next;
+        }
+        
+    }
+
 };
 
-void slide(int arr[], int size, int k){
+void slide(Queue q, int k){
+
+    // plan:
+    //  set the first pointer to point to the kth val in queue
+    //  set the 2nd   pointer to point to the 1st val in queue
+    // then sum all val b/w 1st node and kth node,
+    // move the sum to an array,
+    //      size of arr =  k - q.size
+    // move increment first and 2nd pointer
+    // repeat until kth pointer reaches tail
 
 
+
+    
+    
+    // std::cout << "newArrSize: "<< newArrSize << std::endl;
+    
+    
+    
+    
+    // NEW PLAN
+    // CREATE A QUEUE OF SIZE K
+    // ENQ ALL VAL IN Q, THEN SUM ITS VAL, THEN STORE IN SUM ARRAY
+    
+    Queue kQ(k);
+    
+    int newArrSize = q.size - k; // k is the size of the window, 
+    
+    int* arr = new int[newArrSize];
+    
+    for (size_t i = 0; i < newArrSize; i++)
+    {
+        // arr[i]=0;
+
+        kQ.Enq(q.Deq());
+        std::cout  << std::endl;
+        std::cout << "kQ: " << std::endl;
+        kQ.print();
+        arr[i] = kQ.sum();
+        std::cout << "kqsum: " <<kQ.sum()<< std::endl;
+    }
+
+    cout<<endl;
+    for (size_t i = 0; i < newArrSize; i++)
+    {
+        std::cout << arr[i] <<" ";
+    }
+    
     
 }
 
@@ -91,6 +176,25 @@ void slide(int arr[], int size, int k){
 
 int main(){
 
+    Queue queue(10);
+
+    
+    queue.Enq(9);
+    queue.Enq(8);
+    queue.Enq(7);
+
+    queue.Enq(6);
+    queue.Enq(5);
+    queue.Enq(4);
+
+    queue.Enq(3);
+    queue.Enq(2);
+    queue.Enq(1);
+    // queue.Enq(10);
+
+    // std::cout << queue.population() << std::endl;
+
+    slide(queue,5);
 
 
 
