6acdb6c089385758fad6984b2eaa1c0d778aa799
R4F4I <94185789+R4F4I@users.noreply.github.com>
2025-10-27 10:19:59 +0500

    g

---
+++ DaS/lab/assignments/lab-7/q3.cpp
@@ -0,0 +1,26 @@
+/*
+
+
+
+* Name:         Rafay Siddiqui
+* roll number:  24k-0009
+* section:      BAI-3A
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
