b55f617451897ace1e739ec1b2a3b88b9a127aff
R4F4I <94185789+R4F4I@users.noreply.github.com>
2025-11-19 09:38:14 +0500

    qs

--- DaS/lab/assignments/lab-10/q1.cpp
+++ DaS/lab/assignments/lab-10/q1.cpp
@@ -1,1 +1,17 @@
-lab-
+/* 
+Task#1: Task one create the min max heap from values 8, 7 , 6 ,5 , 4 the tree looks
+like below:
+
+        8
+       / \
+      7   6
+     / \ 
+    5   4
+
+Now change write a function that can take the two parameters one index and other
+value , after that update that value store at that index i with the values provided in
+parameters void update_key(int i, int new_val); also write the delete function
+to delete element from heap.
+
+*/
+
